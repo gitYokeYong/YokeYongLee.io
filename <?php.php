fdb585a99afc68b2caeb33eb7b89dc3ce89647ea
rendy <yokeyongleee@gmail.com>
2024-03-22 15:55:14 +1100

    second update

--- <?php.php
+++ <?php.php
@@ -1,3 +1,75 @@
+mail.php  $errors = [];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['name'], $_POST['email'], $_POST['message']) === true) {
+    $name = filter_var(trim($_POST['name']), FILTER_SANITIZE_STRING);
+
+    if (empty($name) === true) {
+        $errors['name'][] = 'Name is empty';
+    } elseif (ctype_alpha($name) === false) {
+        $errors['name'][] = 'Name contains invalid characters';
+    }
+
+    $email = filter_var(trim($_POST['email']), FILTER_SANITIZE_EMAIL);
+
+    if (empty($email) === true) {
+        $errors['email'][] = 'E-mail is empty';
+    } elseif ($email === false) {
+        $errors['email'][] = 'Invalid e-mail address';
+    }
+
+    $message = strip_tags(trim($_POST['message']));
+
+    if (empty($message) === true) {
+        $errors['message'][] = 'Message is empty';
+    }
+
+    if (count($errors) === 0) {
+        echo 'Thank you!';
+        // you can use here the sanitized user input to send the e-mail
+    }
+}
+
+if (count($errors) > 0) {
+    foreach ($errors as $field => $messages) {
+        echo implode(', ', $messages), '<br>';
+    }
+} <?php $errors = [];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['name'], $_POST['email'], $_POST['message']) === true) {
+    $name = filter_var(trim($_POST['name']), FILTER_SANITIZE_STRING);
+
+    if (empty($name) === true) {
+        $errors['name'][] = 'Name is empty';
+    } elseif (ctype_alpha($name) === false) {
+        $errors['name'][] = 'Name contains invalid characters';
+    }
+
+    $email = filter_var(trim($_POST['email']), FILTER_SANITIZE_EMAIL);
+
+    if (empty($email) === true) {
+        $errors['email'][] = 'E-mail is empty';
+    } elseif ($email === false) {
+        $errors['email'][] = 'Invalid e-mail address';
+    }
+
+    $message = strip_tags(trim($_POST['message']));
+
+    if (empty($message) === true) {
+        $errors['message'][] = 'Message is empty';
+    }
+
+    if (count($errors) === 0) {
+        echo 'Thank you!';
+        // you can use here the sanitized user input to send the e-mail
+    }
+}
+
+if (count($errors) > 0) {
+    foreach ($errors as $field => $messages) {
+        echo implode(', ', $messages), '<br>';
+    }
+} 
+
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST['name'];
@@ -5,7 +77,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $message = $_POST['message'];
 
     // Set your Gmail email address to receive the messages
-    $to = 'yokyongleee@gmail.com';
+    $to = 'yokeyongleee@gmail.com';
 
     // Email subject
     $subject = 'New message from ' . $name;
